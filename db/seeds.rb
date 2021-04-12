013890664b4cd5aeefd3aef8675bddbe22972711
Matoba Taiki <tomatomatobanana@gmail.com>
2021-04-12 16:37:59 +0900

    管理ユーザーの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "的場大輝",
+            password:              "password",
+            password_confirmation: "password")
