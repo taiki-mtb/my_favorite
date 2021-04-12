e3a5b310f89ec36555116a67f8274ec692bb2db1
Matoba Taiki <tomatomatobanana@gmail.com>
2021-04-12 18:00:18 +0900

    login機能の実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(name: params[:session][:name])
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user
+  		redirect_to root_url
+  		flash[:success] = "ログインしました"
+  	else
+  		flash.now[:danger] = "名前とパスワードの組み合わせが間違っています"
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	log_out
+  	redirect_to root_url
+  end
+end
