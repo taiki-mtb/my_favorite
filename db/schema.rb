7a9127d79190c1b69fe5b5f3be40dd44c42fd7ed
Matoba Taiki <tomatomatobanana@gmail.com>
2021-04-12 18:24:58 +0900

    継続ログインの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_072046) do
+ActiveRecord::Schema.define(version: 2021_04_12_090249) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
