5c0017b666458ae4229b5084c79980e20806fc36
Matoba Taiki <tomatomatobanana@gmail.com>
2021-04-14 17:55:38 +0900

    アーティストテスト作成

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -1,7 +1,96 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Artists", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "アーティスト登録", type: :request do
+  let!(:user)  { create(:user) }
+  let!(:artist) { create(:artist) }
+
+  describe "アーティスト登録ページ" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+        get new_artist_path
+      end
+
+      it "レスポンスが正常に表示されること" do
+        expect(response).to render_template('artists/new')
+      end
+
+      it "登録後詳細ページに飛ぶこと" do
+        expect {
+          post artists_path, params: { artist: { name: "name" } }
+        }.to change(Artist, :count).by(1)
+        follow_redirect!
+        expect(response).to render_template('artists/show')
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "トップページにリダイレクトすること" do
+        get new_artist_path
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "アーティスト詳細ページ" do
+    it "レスポンスが正常に表示されること" do
+      login_for_request(user)
+      get artist_path(artist)
+      expect(response).to have_http_status "200"
+      expect(response).to render_template('artists/show')
+    end
+  end
+
+  describe "アーティスト編集ページ" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+        get new_artist_path
+      end
+
+      it "レスポンスが正常に表示されること" do
+        expect(response).to render_template('artists/new')
+      end
+
+      it "編集後詳細ページに飛ぶこと" do
+        patch artist_path(artist), params: { artist: { name: "name" } }
+        redirect_to artist
+        follow_redirect!
+        expect(response).to render_template('artists/show')
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "トップページにリダイレクトすること" do
+        get new_artist_path
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "アーティスト削除" do
+    context "ログインしている場合" do
+      it "処理が成功し、トップページにリダイレクトすること" do
+        login_for_request(user)
+        expect {
+          delete artist_path(artist)
+        }.to change(Artist, :count).by(-1)
+        redirect_to root_url
+        follow_redirect!
+        expect(response).to render_template('top_pages/top')
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "トップページへリダイレクトすること" do
+        expect {
+          delete artist_path(artist)
+        }.not_to change(Artist, :count)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to root_url
+      end
+    end
   end
 end
---
+++ spec/system/artists_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe "Artist", type: :system do
+  let!(:user) { create(:user) }
+  let!(:artist) { create(:artist) }
+
+  describe "アーティスト登録ページ" do
+    before do
+      login_for_system(user)
+      visit new_artist_path
+    end
+
+    context "ページレイアウト" do
+      it "正しいタイトルが表示されること" do
+        expect(page).to have_title full_title('New_artist')
+      end
+
+      it "入力部分に適切なラベルが表示されること" do
+        expect(page).to have_content 'Name'
+        expect(page).to have_content 'Info'
+        expect(page).to have_content 'Image'
+      end
+    end
+
+    context "アーティスト登録処理" do
+      it "有効な情報で登録すると登録成功のフラッシュが表示されること" do
+        fill_in "Name", with: "name"
+        click_button "登録する"
+        expect(page).to have_content "新しいartistが登録されました"
+      end
+
+      it "無効な情報で登録すると登録失敗のフラッシュが表示されること" do
+        fill_in "Name", with: ""
+        click_button "登録する"
+        expect(page).to have_content "Nameを入力してください"
+      end
+    end
+  end
+
+  describe "アーティスト詳細ページ" do
+    before do
+      login_for_system(user)
+      visit artist_path(artist)
+    end
+
+    context "ページレイアウト" do
+      it "正しいタイトルが表示されること" do
+        expect(page).to have_title full_title("#{artist.name}")
+      end
+
+      it "アーティスト情報が表示されること" do
+        expect(page).to have_content artist.name
+        expect(page).to have_content artist.info
+      end
+    end
+
+#    context "アーティストの削除", js: true do
+#      it "削除成功のフラッシュが表示されること" do
+#        within find('.change-artist') do
+#          click_on '削除'
+#        end
+#        page.driver.browser.switch_to.alert.accept
+#        expect(page).to have_content 'artistが削除されました'
+#      end
+#    end
+  end
+end
